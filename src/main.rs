c7a949768f92b96f967e03b571d2e99a7613223f
Tung Nguyen <tungtn3@gmail.com>
2024-01-22 21:32:54 +1100

    close window when esc is pressed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use miniquad::graphics::GraphicsContext;
-use miniquad::EventHandler;
+use miniquad::{EventHandler, KeyCode, KeyMods};
 
 struct App;
 
 impl EventHandler for App {
     fn draw(&mut self, gctx: &mut GraphicsContext) {
         gctx.clear(Some((0.2, 0.3, 0.3, 1.0)), None, None);
+    }
+
+    fn key_down_event(
+        &mut self,
+        gctx: &mut GraphicsContext,
+        keycode: KeyCode,
+        _keymods: KeyMods,
+        _repeat: bool,
+    ) {
+        if keycode == KeyCode::Escape {
+            gctx.request_quit();
+        }
     }
 
     fn update(&mut self, _gctx: &mut GraphicsContext) {}
