ef10c328e837d92f2609d602386346d4f9e01656
Tung Nguyen <tungtn3@gmail.com>
2024-03-18 17:03:21 +1100

    add more encounter groups and assign them to levels

--- src/enemy.rs
+++ src/enemy.rs
@@ -3,9 +3,15 @@ use crate::random::*;
 
 #[derive(Clone, Copy)]
 pub enum EncounterGroup {
-    Forest1,
+    Wilderness1,
+    Wilderness2,
+    Wilderness3,
+    EarthCastle,
+    WaterCastle,
+    FireCastle,
 }
 
+#[derive(Clone)]
 pub struct Enemy {
     pub name: &'static str,
     pub sprite_path: &'static str,
@@ -16,38 +22,102 @@ pub struct Enemy {
     pub exp: i32,
 }
 
+const ENEMIES_WILDERNESS1: &[Enemy] = &[
+    Enemy {
+        name: "Rat",
+        sprite_path: "rat.png",
+        hp: 33,
+        attack: 8,
+        defense: 5,
+        weakness: Some(Magic::FireEdge),
+        exp: 5,
+    },
+    Enemy {
+        name: "Dog",
+        sprite_path: "dog.png",
+        hp: 50,
+        attack: 10,
+        defense: 7,
+        weakness: Some(Magic::FireEdge),
+        exp: 5,
+    },
+];
+
+const ENEMIES_WILDERNESS2: &[Enemy] = &[Enemy {
+    name: "Rat Lv8",
+    sprite_path: "rat.png",
+    hp: 96,
+    attack: 25,
+    defense: 22,
+    weakness: Some(Magic::FireEdge),
+    exp: 5,
+}];
+
+const ENEMIES_WILDERNESS3: &[Enemy] = &[Enemy {
+    name: "Rat Lv16",
+    sprite_path: "rat.png",
+    hp: 162,
+    attack: 47,
+    defense: 44,
+    weakness: Some(Magic::FireEdge),
+    exp: 5,
+}];
+
+const ENEMIES_EARTH_CASTLE: &[Enemy] = &[Enemy {
+    name: "Rat Lv5",
+    sprite_path: "rat.png",
+    hp: 74,
+    attack: 19,
+    defense: 16,
+    weakness: Some(Magic::FireEdge),
+    exp: 5,
+}];
+
+const ENEMIES_WATER_CASTLE: &[Enemy] = &[Enemy {
+    name: "Rat Lv10",
+    sprite_path: "rat.png",
+    hp: 126,
+    attack: 35,
+    defense: 32,
+    weakness: Some(Magic::FireEdge),
+    exp: 5,
+}];
+
+const ENEMIES_FIRE_CASTLE: &[Enemy] = &[Enemy {
+    name: "Rat Lv20",
+    sprite_path: "rat.png",
+    hp: 222,
+    attack: 67,
+    defense: 64,
+    weakness: Some(Magic::FireEdge),
+    exp: 5,
+}];
+
 impl EncounterGroup {
     pub fn random_enemy(self) -> Enemy {
-        let Self::Forest1 = self;
-        let r = random(2);
-        if r == 0 {
-            Enemy {
-                name: "Rat",
-                sprite_path: "rat.png",
-                hp: 52,
-                attack: 5,
-                defense: 5,
-                weakness: Some(Magic::FireEdge),
-                exp: 5,
-            }
-        } else {
-            Enemy {
-                name: "Dog",
-                sprite_path: "dog.png",
-                hp: 52,
-                attack: 5,
-                defense: 5,
-                weakness: Some(Magic::FireEdge),
-                exp: 5,
-            }
-        }
+        let group = match self {
+            Self::Wilderness1 => ENEMIES_WILDERNESS1,
+            Self::Wilderness2 => ENEMIES_WILDERNESS2,
+            Self::Wilderness3 => ENEMIES_WILDERNESS3,
+            Self::EarthCastle => ENEMIES_EARTH_CASTLE,
+            Self::WaterCastle => ENEMIES_WATER_CASTLE,
+            Self::FireCastle => ENEMIES_FIRE_CASTLE,
+        };
+        assert!(!group.is_empty());
+        let i = u32::try_from(group.len()).expect("u32 enemy group length");
+        group[random(i) as usize].clone()
     }
 }
 
 impl From<&str> for EncounterGroup {
     fn from(s: &str) -> Self {
         match s {
-            "Forest1" => Self::Forest1,
+            "Wilderness1" => Self::Wilderness1,
+            "Wilderness2" => Self::Wilderness2,
+            "Wilderness3" => Self::Wilderness3,
+            "EarthCastle" => Self::EarthCastle,
+            "WaterCastle" => Self::WaterCastle,
+            "FireCastle" => Self::FireCastle,
             _ => panic!("unknown encounter group: {s}"),
         }
     }
