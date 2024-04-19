3666fff0be7f1a7a7074dbffa510f220a14a8477
Tung Nguyen <tungtn3@gmail.com>
2024-04-19 18:16:58 +1000

    redo enemy stats and experience values
    With the upcoming experience curve, less battles are needed to gain
    levels early, which means the level of the player at any given point in
    the game is higher than was expected before.

--- src/enemy.rs
+++ src/enemy.rs
@@ -43,172 +43,43 @@ const ENEMIES_WILDERNESS1: &[Enemy] = &[
         weakness: None,
         exp: 15,
     },
-    // level 2.5
+    // level 3
     Enemy {
         name: "Horn Beast",
         sprite_path: "horn-beast.png",
-        hp: 46,
-        attack: 8,
-        defense: 5,
-        weakness: None,
-        exp: 18,
-    },
-    // level 3
-    Enemy {
-        name: "Cobra",
-        sprite_path: "cobra.png",
         hp: 51,
         attack: 9,
         defense: 6,
         weakness: None,
         exp: 22,
     },
-];
-
-const ENEMIES_WILDERNESS2: &[Enemy] = &[
-    // level 7
-    Enemy {
-        name: "Dragonfly",
-        sprite_path: "dragonfly.png",
-        hp: 88,
-        attack: 19,
-        defense: 16,
-        weakness: Some(Magic::FireEdge),
-        exp: 67,
-    },
-    // level 8
-    Enemy {
-        name: "Leech",
-        sprite_path: "leech.png",
-        hp: 98,
-        attack: 22,
-        defense: 18,
-        weakness: None,
-        exp: 80,
-    },
-    // level 9
-    Enemy {
-        name: "Shambler",
-        sprite_path: "shambler.png",
-        hp: 107,
-        attack: 24,
-        defense: 21,
-        weakness: None,
-        exp: 96,
-    },
-    // level 9.5
-    Enemy {
-        name: "Fang Frog",
-        sprite_path: "fang-frog.png",
-        hp: 112,
-        attack: 25,
-        defense: 22,
-        weakness: None,
-        exp: 106,
-    },
-];
-
-const ENEMIES_WILDERNESS3: &[Enemy] = &[
-    // level 13
-    Enemy {
-        name: "Griffon",
-        sprite_path: "griffon.png",
-        hp: 144,
-        attack: 34,
-        defense: 31,
-        weakness: Some(Magic::EarthEdge),
-        exp: 199,
-    },
-    // level 14
-    Enemy {
-        name: "Orc",
-        sprite_path: "orc.png",
-        hp: 154,
-        attack: 37,
-        defense: 33,
-        weakness: None,
-        exp: 239,
-    },
-    // level 16
-    Enemy {
-        name: "Troll",
-        sprite_path: "troll.png",
-        hp: 172,
-        attack: 42,
-        defense: 38,
-        weakness: Some(Magic::WaterEdge),
-        exp: 345,
-    },
-    // level 18
-    Enemy {
-        name: "War Tusk",
-        sprite_path: "war-tusk.png",
-        hp: 191,
-        attack: 46,
-        defense: 44,
-        weakness: None,
-        exp: 496,
-    },
-];
-
-const ENEMIES_EARTH_CASTLE: &[Enemy] = &[
-    // level 3.5
-    Enemy {
-        name: "Bat",
-        sprite_path: "bat.png",
-        hp: 56,
-        attack: 10,
-        defense: 7,
-        weakness: None,
-        exp: 26,
-    },
     // level 4
     Enemy {
-        name: "Scorpion",
-        sprite_path: "scorpion.png",
+        name: "Cobra",
+        sprite_path: "cobra.png",
         hp: 60,
         attack: 12,
         defense: 8,
         weakness: None,
         exp: 30,
     },
-    // level 5
-    Enemy {
-        name: "Rogue",
-        sprite_path: "rogue.png",
-        hp: 70,
-        attack: 14,
-        defense: 11,
-        weakness: None,
-        exp: 41,
-    },
-    // level 6
-    Enemy {
-        name: "Golem",
-        sprite_path: "golem.png",
-        hp: 79,
-        attack: 16,
-        defense: 14,
-        weakness: Some(Magic::FireEdge),
-        exp: 53,
-    },
 ];
 
-const ENEMIES_WATER_CASTLE: &[Enemy] = &[
+const ENEMIES_WILDERNESS2: &[Enemy] = &[
     // level 9
     Enemy {
-        name: "Jelly",
-        sprite_path: "jelly.png",
+        name: "Dragonfly",
+        sprite_path: "dragonfly.png",
         hp: 107,
         attack: 24,
         defense: 21,
-        weakness: None,
+        weakness: Some(Magic::FireEdge),
         exp: 96,
     },
     // level 10
     Enemy {
-        name: "Ghost",
-        sprite_path: "ghost.png",
+        name: "Leech",
+        sprite_path: "leech.png",
         hp: 116,
         attack: 27,
         defense: 23,
@@ -217,8 +88,8 @@ const ENEMIES_WATER_CASTLE: &[Enemy] = &[
     },
     // level 11
     Enemy {
-        name: "Turtle",
-        sprite_path: "turtle.png",
+        name: "Shambler",
+        sprite_path: "shambler.png",
         hp: 126,
         attack: 29,
         defense: 26,
@@ -227,46 +98,175 @@ const ENEMIES_WATER_CASTLE: &[Enemy] = &[
     },
     // level 12
     Enemy {
-        name: "Serpent",
-        sprite_path: "serpent.png",
+        name: "Fang Frog",
+        sprite_path: "fang-frog.png",
         hp: 135,
         attack: 32,
         defense: 28,
-        weakness: Some(Magic::EarthEdge),
+        weakness: None,
         exp: 166,
     },
 ];
 
-const ENEMIES_FIRE_CASTLE: &[Enemy] = &[
+const ENEMIES_WILDERNESS3: &[Enemy] = &[
+    // level 17
+    Enemy {
+        name: "Griffon",
+        sprite_path: "griffon.png",
+        hp: 182,
+        attack: 44,
+        defense: 41,
+        weakness: Some(Magic::EarthEdge),
+        exp: 414,
+    },
+    // level 18
+    Enemy {
+        name: "Orc",
+        sprite_path: "orc.png",
+        hp: 191,
+        attack: 47,
+        defense: 43,
+        weakness: None,
+        exp: 496,
+    },
     // level 19
     Enemy {
-        name: "Basilisk",
-        sprite_path: "basilisk.png",
+        name: "Troll",
+        sprite_path: "troll.png",
         hp: 200,
         attack: 49,
         defense: 46,
-        weakness: None,
+        weakness: Some(Magic::WaterEdge),
         exp: 596,
     },
     // level 20
     Enemy {
-        name: "Warlock",
-        sprite_path: "warlock.png",
+        name: "War Tusk",
+        sprite_path: "war-tusk.png",
         hp: 210,
-        attack: 52,
-        defense: 48,
+        attack: 51,
+        defense: 49,
         weakness: None,
         exp: 715,
     },
+];
+
+const ENEMIES_EARTH_CASTLE: &[Enemy] = &[
+    // level 5
+    Enemy {
+        name: "Bat",
+        sprite_path: "bat.png",
+        hp: 70,
+        attack: 14,
+        defense: 11,
+        weakness: None,
+        exp: 41,
+    },
+    // level 6
+    Enemy {
+        name: "Scorpion",
+        sprite_path: "scorpion.png",
+        hp: 79,
+        attack: 16,
+        defense: 14,
+        weakness: None,
+        exp: 53,
+    },
+    // level 7
+    Enemy {
+        name: "Rogue",
+        sprite_path: "rogue.png",
+        hp: 88,
+        attack: 19,
+        defense: 16,
+        weakness: None,
+        exp: 67,
+    },
+    // level 8
+    Enemy {
+        name: "Golem",
+        sprite_path: "golem.png",
+        hp: 98,
+        attack: 22,
+        defense: 19,
+        weakness: Some(Magic::FireEdge),
+        exp: 80,
+    },
+];
+
+const ENEMIES_WATER_CASTLE: &[Enemy] = &[
+    // level 13
+    Enemy {
+        name: "Jelly",
+        sprite_path: "jelly.png",
+        hp: 144,
+        attack: 34,
+        defense: 31,
+        weakness: None,
+        exp: 199,
+    },
+    // level 14
+    Enemy {
+        name: "Ghost",
+        sprite_path: "ghost.png",
+        hp: 154,
+        attack: 37,
+        defense: 33,
+        weakness: None,
+        exp: 239,
+    },
+    // level 15
+    Enemy {
+        name: "Turtle",
+        sprite_path: "turtle.png",
+        hp: 163,
+        attack: 39,
+        defense: 66,
+        weakness: None,
+        exp: 287,
+    },
+    // level 16
+    Enemy {
+        name: "Serpent",
+        sprite_path: "serpent.png",
+        hp: 172,
+        attack: 42,
+        defense: 39,
+        weakness: Some(Magic::EarthEdge),
+        exp: 345,
+    },
+];
+
+const ENEMIES_FIRE_CASTLE: &[Enemy] = &[
+    // level 21
+    Enemy {
+        name: "Basilisk",
+        sprite_path: "basilisk.png",
+        hp: 219,
+        attack: 54,
+        defense: 51,
+        weakness: None,
+        exp: 858,
+    },
     // level 22
     Enemy {
-        name: "Minotaur",
-        sprite_path: "minotaur.png",
+        name: "Warlock",
+        sprite_path: "warlock.png",
         hp: 228,
         attack: 57,
         defense: 53,
         weakness: None,
         exp: 1029,
+    },
+    // level 23
+    Enemy {
+        name: "Minotaur",
+        sprite_path: "minotaur.png",
+        hp: 238,
+        attack: 59,
+        defense: 56,
+        weakness: None,
+        exp: 1235,
     },
     // level 24
     Enemy {
@@ -274,7 +274,7 @@ const ENEMIES_FIRE_CASTLE: &[Enemy] = &[
         sprite_path: "vampire.png",
         hp: 247,
         attack: 62,
-        defense: 58,
+        defense: 59,
         weakness: Some(Magic::WaterEdge),
         exp: 1482,
     },
